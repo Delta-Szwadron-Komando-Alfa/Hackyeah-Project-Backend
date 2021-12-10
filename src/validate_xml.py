f35e5ba4da88f4b3f66d641e531375f687ff67bc
Wojtek Papliński <wojtekpaplinski@gmail.com>
2021-12-10 22:47:36 +0100

    Changed function arguments names

--- src/validate_xml.py
+++ src/validate_xml.py
@@ -1,15 +1,15 @@
 from lxml import etree
 
 
-def validate_xml_with_xsd(xml_path, xsd_path) -> bool:
+def validate_xml_with_xsd(xml_file, xsd_file) -> bool:
     """
     You can pass either paths to the files
     or you can pass files opened in Python
     """
-    xmlschema_doc = etree.parse(xsd_path)
+    xmlschema_doc = etree.parse(xsd_file)
     xmlschema = etree.XMLSchema(xmlschema_doc)
 
-    xml_doc = etree.parse(xml_path)
+    xml_doc = etree.parse(xml_file)
     result = xmlschema.validate(xml_doc)
 
     return result
