894ec4d15a294bcbf401a8bb7f8b49def3d17e61
Wojtek Papliński <wojtekpaplinski@gmail.com>
2021-12-12 07:22:17 +0100

    Validate endpoint, fix for cors

--- src/api/main.py
+++ src/api/main.py
@@ -1,63 +1,33 @@
 import json
-from mmap import ALLOCATIONGRANULARITY
 import shutil
 import uvicorn
 from typing import List
 from pathlib import Path
 from uuid import uuid4
+from lxml import etree
+import requests
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import RedirectResponse
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 from droid.droid_handler import Client
+from validate_xml import validate_xml_with_xsd
 
-app = FastAPI()
-
-app.add_middleware(
+middleware = [Middleware(
     CORSMiddleware,
     allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*']
-)
+)]
 
-sig_dict = {}
-ext_dict = {}
-
-# Open two jsons and read their contents into the dictionary
-with open('signatures.json', 'r') as file:
-    sig_dict = json.load(file)
-
-with open('extensions.json', 'r') as file:
-    ext_dict = json.load(file)
+app = FastAPI(middleware=middleware)
 
 @app.get('/')
 async def root():
     return RedirectResponse('/docs')
-
-# Extensions
-@app.get('/extensions')
-async def get_all_signatures():
-    return ext_dict
-
-@app.get('/extensions/{extension}')
-async def get_signature(extension):
-    extension = extension.lower()
-    signature = ext_dict.get(extension.lower())
-    return {extension: signature}
-###
-
-# Signatures
-@app.get('/signatures')
-async def get_all_extensions():
-    return sig_dict
-
-@app.get('/signatures/{signature}')
-async def get_extension(signature):
-    extension = sig_dict.get(signature)
-    return {signature: extension}
-###
 
 # Files format
 @app.post('/upload')
@@ -74,6 +44,37 @@ async def upload(files: List[UploadFile] = File(...)):
     
     return json.loads(output)
 ###
+
+# Validate XML
+@app.post('/validate')
+async def validate(file: UploadFile = File(...)):
+    random_path = f'temp/{uuid4().hex}'
+    Path(random_path).mkdir(parents=True, exist_ok=True)
+
+    filepath = f'{random_path}/file.xml' 
+
+    with open(filepath, 'wb+') as file_object:
+        file_object.write(file.file.read())
+
+    xml_doc = etree.parse(filepath)
+    nsmap = {}
+    for ns in xml_doc.xpath('//namespace::*'):
+        if ns[0]: # Removes the None namespace, neither needed nor supported.
+            nsmap[ns[0]] = ns[1]
+
+    schema_location = xml_doc.xpath('//wnio:Dokument', namespaces=nsmap)[0].attrib
+    schema_location = [x for x in str(schema_location).split() if '.xsd' in x][0]
+
+    resp = requests.get(schema_location, allow_redirects=True)
+    cont = resp.content.decode('utf-8')
+    
+    if 'nie posiada pliku' not in cont:
+        with open(f'{random_path}/file.xsd', 'wb+') as file_object:
+            file_object.write(resp.content)
+        resp = validate_xml_with_xsd(filepath, f'{random_path}/file.xsd')
+        return {file.filename: resp}
+
+    return {file.filename: 'No schema to validate'}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8000, host='0.0.0.0')
